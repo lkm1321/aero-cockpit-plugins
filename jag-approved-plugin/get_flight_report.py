6717798003c1009b79938a0b2ef96d54f6cd04f5
lkm1321 <lkm1321@gmail.com>
2017-11-17 03:12:33 +1100

    Make it a bit faster

--- jag-approved-plugin/get_flight_report.py
+++ jag-approved-plugin/get_flight_report.py
@@ -12,7 +12,7 @@ def main():
 
 	parser = argparse.ArgumentParser(description="Get flight time and battery voltages")
 	parser.add_argument('logs_path', default='/var/lib/mavlink_router')
-	parser.add_argument('num_recent', default="5", type=int, nargs='?')
+	parser.add_argument('num_recent', default="1", type=int, nargs='?')
 
 	args = parser.parse_args()
 
@@ -32,60 +32,76 @@ def main():
 		num_recent_san = args.num_recent if args.num_recent is not None else 5
 		num_files = min(len(ulog_file_list), int(num_recent_san)) 
 
-		cur_file_idx = -1
+		# Start from the one before. 
+		cur_file_idx = -2
 
 		while num_files > 0:
-
 			ulog_file = ulog_file_list[cur_file_idx]
 
 			# Decrement file count iff it's a valid log. 
 			if get_flight_report(args.logs_path + '/'+ ulog_file) is True:
 				num_files = num_files - 1
 
+		#	print(num_files)
 			cur_file_idx = cur_file_idx - 1
+
 def get_flight_report(ulog_file):
 
+	print(ulog_file)
 	try:
+		
 		ulog = ULog(ulog_file, ['vehicle_status', 'battery_status', 'vehicle_gps_position'])
 		vehicle_status = ulog.get_dataset('vehicle_status')
 		battery_status = ulog.get_dataset('battery_status')
-		print('Log file name: {0}'.format(ulog_file))
-		try: 
-			gps_position = ulog.get_dataset('vehicle_gps_position')
-			start_time = time.localtime(gps_position.data['time_utc_usec'][0]/1E6) # in us since epoch
-			# boot_time = time.localtime(gps_position.data['timestamp'][0]/1E6) # in us since FC boot
-			print('System powered on {0}'.format(time.asctime(start_time)))
-
-		except:
-			print('No GPS. No UTC time')
-
+		utc_avail = False		
 		# Processing arming data. 
 
 		armed_data = vehicle_status.data['arming_state'] # 1 is standby, 2 is armed. 
 		armed_tstamp = vehicle_status.data['timestamp']  # in us. 
+
+		was_armed = np.any(armed_data == 2)
+
+		if not was_armed:
+			return False
+
+		try: 
+			gps_position = ulog.get_dataset('vehicle_gps_position')
+			start_time = time.localtime(gps_position.data['time_utc_usec'][0]/1E6) # in us since epoch
+			# boot_time = time.localtime(gps_position.data['timestamp'][0]/1E6) # in us since FC boot
+			utc_avail = True
+		except:
+			utc_avail = False
+
 
 		batt_v_start = battery_status.data['voltage_filtered_v'][0]
 		batt_v_end = battery_status.data['voltage_filtered_v'][-1]
 		# batt_v_tsamp = battery_status.data['timestamp'] # in us. 
 
 		arm_disarm_idx = np.where(armed_data[:-1] != armed_data[1:]) # Find where arming state changes by comparing to neighbor. 
-		
-		if (len(arm_disarm_idx) > 0):
-			arm_disarm_events = armed_data[arm_disarm_idx]
-			arm_disarm_events_tstamp = armed_tstamp[arm_disarm_idx]
-			arm_disarm_durations = ( arm_disarm_events_tstamp[1::2] - arm_disarm_events_tstamp[0::2] ) / 1E6 # Assume every second event is arming. Can't be armed on boot!
-			arm_disarm_total_time = arm_disarm_durations.sum()
-			arm_min, arm_sec = divmod(arm_disarm_total_time, 60)
-			arm_min = int(arm_min) # Because it's an integer. 
-			print('Arm duration: {0} min {1} sec'.format( str(arm_min), str(arm_sec) ))
-			print('Start voltage: {0}V, End voltage: {1}V'.format(str(batt_v_start), str(batt_v_end)))
-		else: 
-			print('No arming')
+		arm_disarm_events = armed_data[arm_disarm_idx]
+		arm_disarm_events_tstamp = armed_tstamp[arm_disarm_idx]
+		arm_disarm_durations = ( arm_disarm_events_tstamp[1::2] - arm_disarm_events_tstamp[0::2] ) / 1E6 # Assume every second event is arming. Can't be armed on boot!
+		arm_disarm_total_time = arm_disarm_durations.sum()
+		arm_min, arm_sec = divmod(arm_disarm_total_time, 60)
+		arm_min = int(arm_min) # because it's an integer. 
+
+		if utc_avail:
+			print('System powered on {0}'.format(time.asctime(start_time)))
+		else:
+			print('No GPS. No UTC time')
+		print('Log file name: {0}'.format(ulog_file))
+		print('Arm duration: {0} min {1} sec'.format( str(arm_min), str(arm_sec) ))
+		print('Start voltage: {0}V, End voltage: {1}V'.format(str(batt_v_start), str(batt_v_end)))
 		print('\n\n')
 		return True
 
-	except:
-		# print('Empty or invalid file')
+	except IndexError:
+	#	print('Empty or invalid file')
 		return False
+	except Exception as e: 
+	# 	print(str(e))
+		return False 
+
+
 
 main()
